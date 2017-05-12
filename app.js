c717e4a1f25ec0967c4df80695ac794748fc69bb
Saule BN <beyonce@thinkful.com>
2017-05-12 17:21:12 -0400

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //appState
 const appState = {
-  currentIndex: 0,
-  items:[]
+    currentIndex: 0,
+    items:[]
 };
 
 // let test= function (item) {
@@ -20,25 +20,31 @@ const appState = {
 
 //state mod functions
 const addItem = function(state, item) {
-  state.currentIndex++
-  state.items.push({
-    title: item,
-    done: false,
-    index: state.currentIndex,
+    state.currentIndex++
+    state.items.push({
+      title: item,
+      done: false,
+      index: state.currentIndex,
   });
 };
 const itemDone = function(state, index) {
-  console.log(typeof index);
-  let obj = state.items.find(item => item.index === index);
-  console.log(obj);
-  return obj.done = true;
+  //console.log(typeof index);
+    let obj = state.items.find(item => item.index === index);
+  //console.log(obj);
+    return obj.done = true;
+};
+
+const deleteItem = function(state, index){
+    let obj = state.items.find(item => item.index === index);
+    let i= state.items.indexOf(obj);
+    return state.items.splice(i,1);
 };
 
 //render functions
 const renderList = function(state, element) {
-  const itemsHTML = state.items.map(function(item) {
-    return `<li>
-             <span class="shopping-item ${(!item.done) ? null : 'shopping-item__checked'}">${item.title}</span>
+    const itemsHTML = state.items.map(function(item) {
+      return `<li>
+             <span class="shopping-item ${(item.done) ? 'shopping-item__checked' : null }">${item.title}</span>
              <div class="shopping-item-controls" id=${item.index}>
                <button class="shopping-item-toggle">
                 <span class="button-label">check</span>
@@ -49,7 +55,7 @@ const renderList = function(state, element) {
              </div>
             </li>`;
   });
-  element.html(itemsHTML);
+    element.html(itemsHTML);
 };
 
 //event listeners(function)
@@ -60,11 +66,16 @@ $('#js-shopping-list-form').submit(function(event){
     $(this)[0].reset();
 });
 $('.shopping-list').on('click', 'button.shopping-item-toggle', function(event) {
-  const idBtn = Number($(event.currentTarget).closest('div.shopping-item-controls').attr('id'));
-  itemDone(appState, idBtn);
-  renderList(appState, $('.shopping-list'));
+    const idBtn = Number($(event.currentTarget).closest('div.shopping-item-controls').attr('id'));
+    itemDone(appState, idBtn);
+    renderList(appState, $('.shopping-list'));
 });
 
+$('.shopping-list').on('click','.shopping-item-delete', function(event) {
+   const idBtn = Number($(event.currentTarget).closest('div.shopping-item-controls').attr('id'));
+   deleteItem(appState, idBtn);
+   renderList(appState, $('.shopping-list'));
+});
 
 
 // addItem(appState, ${input}.val());
