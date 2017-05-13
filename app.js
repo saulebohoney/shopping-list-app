6b1bebfffdcae57dc07f757c9f42d752b242f45a
tckastanek <tckastanek@gmail.com>
2017-05-12 22:05:13 -0400

    extra features added

--- app.js
+++ app.js
@@ -1,58 +1,73 @@
 //appState
 const appState = {
-    currentIndex: 0,
+    currentID: 0,
     items:[]
 };
 
-// let test= function (item) {
-//     return`<li>
-//         <span class="shopping-item">${item}</span>
-//         <div class="shopping-item-controls">
-//           <button class="shopping-item-toggle">
-//             <span class="button-label">check</span>
-//           </button>
-//           <button class="shopping-item-delete">
-//             <span class="button-label">delete</span>
-//           </button>
-//         </div>
-//       </li>`;
-// };
-
 //state mod functions
 const addItem = function(state, item) {
-    state.currentIndex++
+    state.currentID++
     state.items.push({
       title: item,
       done: false,
-      index: state.currentIndex,
+      hidden: false,
+      itemID: state.currentID,
   });
 };
-const itemToggle = function(state, index) {
-  let obj = state.items.find(item => item.index === index);
+const itemToggle = function(state, domID) {
+  let obj = state.items.find(item => item.itemID === domID);
   return (obj.done) ? (obj.done = false) : (obj.done = true);
 };
-const deleteItem = function(state, index){
-    let obj = state.items.find(item => item.index === index);
-    let i= state.items.indexOf(obj);
-    return state.items.splice(i,1);
+const deleteItem = function(state, domID){
+    let obj = state.items.find(item => item.itemID === domID);
+    let i = state.items.indexOf(obj);
+    return state.items.splice(i , 1);
 };
+const editItem = function(state, domID, nTitle) {
+  let obj = state.items.find(item => item.itemID === domID);
+  return obj.title = nTitle;
+};
+const showItem = function (state, domName) {
+  let altObj = state.items.filter(item => item.title === domName);
+  return altObj.map(item => item.hidden = false);
+};
+const hideItem = function(state, domID) {
+  let obj = state.items.find(item => item.itemID === domID);
+  return obj.hidden = true;
+};
+const showAll = function(state) {
+  return state.items.map(item => item.hidden = false);
+};
+const hideAll = function(state) {
+  return state.items.map(item => item.hidden = true);
+}
 
 //render functions
 const renderList = function(state, element) {
-    const itemsHTML = state.items.map(function(item) {
-      return `<li>
-             <span class="shopping-item ${(item.done) ? 'shopping-item__checked' : null }">${item.title}</span>
-             <div class="shopping-item-controls" id=${item.index}>
+  const itemsHTML = state.items.map(function(item) {
+    return `<li class="todo-list-item" ${(item.hidden) ? 'hidden' : null}>
+             <span class="shopping-item ${(item.done) ? 'shopping-item__checked' : null}">${item.title}</span>
+             <div class="shopping-item-controls" id=${item.itemID}>
                <button class="shopping-item-toggle">
                 <span class="button-label">check</span>
                </button>
                <button class="shopping-item-delete">
                 <span class="button-label">delete</span>
                </button>
+               <button class="hide-me-button" ${(item.done) ? null : 'hidden'}>
+                <span class="button-label">hide</span>
+               </button>
+             </div>
+             <div class="item-edit-panel" id=${item.itemID}>
+             <form id="js-edit-form">
+               <label for="shopping-list-edit"></label>
+               <input type="text" name="shopping-list-edit" id="shopping-list-edit" placeholder="rename your entry">
+               <button type="submit">Edit item</button>
+             </form>
              </div>
             </li>`;
   });
-    element.html(itemsHTML);
+  element.html(itemsHTML);
 };
 
 //event listeners(function)
@@ -67,12 +82,32 @@ $('.shopping-list').on('click', 'button.shopping-item-toggle', function(event) {
     itemToggle(appState, idBtn);
     renderList(appState, $('.shopping-list'));
 });
-
 $('.shopping-list').on('click','button.shopping-item-delete', function(event) {
    const idBtn = Number($(event.currentTarget).closest('div.shopping-item-controls').attr('id'));
    deleteItem(appState, idBtn);
    renderList(appState, $('.shopping-list'));
 });
-
-
-// addItem(appState, ${input}.val());
+$('.shopping-list').on('click','button.hide-me-button', function(event) {
+   const idBtn = Number($(event.currentTarget).closest('div.shopping-item-controls').attr('id'));
+   hideItem(appState, idBtn);
+   renderList(appState, $('.shopping-list'));
+});
+$('.shopping-list').on('submit', '#js-edit-form', function(event) {
+  event.preventDefault();
+  const idBtn = Number($(event.currentTarget).closest('div.item-edit-panel').attr('id'));
+  const editVal = $('#shopping-list-edit').closest('input').val();
+  editItem(appState, idBtn, editVal);
+  renderList(appState, $('.shopping-list'));
+});
+$('#js-search-form').submit(function(event) {
+  event.preventDefault();
+  let srchVal = $('#shopping-list-search').val();
+  hideAll(appState);
+  showItem(appState, srchVal);
+  renderList(appState, $('.shopping-list'));
+  $(this)[0].reset();
+})
+$('.show-all-button').click(function(event) {
+  showAll(appState);
+  renderList(appState, $('.shopping-list'));
+});
